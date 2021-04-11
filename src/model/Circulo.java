d2d07abe36ebeda57ee742334b2435158eff3e25
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2021-04-11 12:54:43 -0500

    Las figuras se pintan con color aleatorio y se nota el valor

--- src/model/Circulo.java
+++ src/model/Circulo.java
@@ -6,17 +6,19 @@ public class Circulo extends Figura{
 
 	public Circulo(String tipo, int tam, int posX, int posY, int dir, int valor, PApplet app) {
 		super(tipo, tam, posX, posY, dir, valor, app);
+		fijarColor();
 	}
 	
 	@Override
 	protected void pintarFigura() {
-		app.fill(0);
+		app.noStroke();
+		app.fill(r, g, b);
 		app.circle(posX, posY, tam);
-		app.stroke(0);
+		app.fill(0);
+		app.stroke(255);
 		app.strokeWeight(2);
 		app.textSize(14);
-		app.fill(255);
-		app.text(valor, posX, posY+8);
+		app.text(valor, posX, posY);
 		
 	}
 	
--- src/model/Cuadrado.java
+++ src/model/Cuadrado.java
@@ -6,18 +6,20 @@ public class Cuadrado extends Figura{
 
 	public Cuadrado(String tipo, int tam, int posX, int posY, int dir, int valor, PApplet app) {
 		super(tipo, tam, posX, posY, dir, valor, app);
+		fijarColor();
 	}
 	
 
 	@Override
 	protected void pintarFigura() {
-		app.fill(0);
+		app.noStroke();
+		app.fill(r, g, b);
 		app.square(posX, posY, tam);
-		app.stroke(0);
+		app.fill(0);
+		app.stroke(255);
 		app.strokeWeight(2);
 		app.textSize(14);
-		app.fill(255);
-		app.text(valor, posX, posY+8);
+		app.text(valor, posX, posY+10);
 		
 	}
 
